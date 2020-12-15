abe2d2b64dd6c43b32b2051b6da4582eecbd1c80
conjonpo <aardwolf57@gmail.com>
2020-12-14 18:20:42 -0700

    commit

---
+++ app.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", event => {
+
+    const app = firebase.app();
+
+    const db = firebase.firestore();
+
+    const myPost = db.collection('posts').doc('firstpost')
+
+    myPost.onSnapshot(doc => {
+
+        const data = doc.data();
+        document.querySelector('#title').innerHTML = data.title
+
+    })
+
+});
+
+function updatePost(e) {
+    const db = firebase.firestore();
+    const myPost = db.collection('posts').doc('firstpost');
+    myPost.update({ title: e.target.value })
+}
